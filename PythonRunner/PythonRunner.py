e867fe142e37b3d2eb1ac825cbfe93f76c9b25a3
Gino Vincenzini <OpenMySourceCode@gmail.com>
2020-03-05 14:02:41 -0500

    Uncommented lines in python project; removed extraneous pass statements.

--- PythonRunner/PythonRunner.py
+++ PythonRunner/PythonRunner.py
@@ -5,17 +5,17 @@ from enum import Enum
 
 lib = cdll.LoadLibrary("./CSharp To Python Proof of Concept.dll")
 msgbox = lib.safeMessageBox
-# result = msgbox("Hello!", "Hello From Python to C#!")
+result = msgbox("Hello!", "Hello From Python to C#!")
 getTrue = lib.getTrue
 getFalse = lib.getFalse
 
 if getTrue():
-#	msgbox("True!", "True from C# retrieved successfully")
-	pass
+	msgbox("True!", "True from C# retrieved successfully")
+
 
 if not getFalse():
-#	msgbox("False!", "False from C# retrieved successfully")
-	pass
+	msgbox("False!", "False from C# retrieved successfully")
+
 
 class DialogResult(Enum):
     Abort = 3
@@ -27,9 +27,9 @@ class DialogResult(Enum):
     Retry = 4
     Yes = 6
 
-# if DialogResult(result) == DialogResult.OK:
-# 	msgbox("Dialog Result", "OK Button was clicked!")
-# 	pass
+if DialogResult(result) == DialogResult.OK:
+	msgbox("Dialog Result", "OK Button was clicked!")
+
 
 lib.getText.restype=c_wchar_p
 getText = lib.getText;
